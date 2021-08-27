0833f5d0d5ff7ec74e6e3bbea9f993aeaa849f03
ruiborda <bordarui@gmail.com>
2021-08-27 11:57:44 -0500

    docs

---
+++ docs/earth.png.proxy.js
@@ -0,0 +1,1 @@
+export default "/earth.png";
---
+++ docs/objects/markPosition.js
@@ -0,0 +1,8 @@
+import * as THREE from "../snowpack/pkg/three.js";
+
+const geometry = new THREE.SphereGeometry(5, 64, 64);
+const material = new THREE.MeshBasicMaterial({
+    color: 0xffffff,
+    transparent: false
+})
+export const markPosition = new THREE.Mesh(geometry, material)
